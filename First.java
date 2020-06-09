d815d36d0fa46794a44bc7ddb304369da83c080d
Arpit81 <63946976+Arpit81@users.noreply.github.com>
2020-06-09 13:25:10 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,13 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		
+		String name = new String("Arpit Mangal");
+		System.out.println(name.hashCode());
+		System.out.println(name.intern().hashCode());
+
+	}
+
+}
