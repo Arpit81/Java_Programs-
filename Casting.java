553f1406da91bf202a24b192cbc6c3a1c2994639
Arpit Mangal <63946976+Arpit81@users.noreply.github.com>
2020-06-28 09:30:04 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,21 @@
+
+
+public class Casting {
+
+	public static void main(String[] args)
+	{
+		int i=29;
+		System.out.println(i);
+		double j= i;
+		System.out.println(j);
+		float f=i;
+		System.out.println(f);
+		byte b=(byte)i;
+		System.out.println(b);
+		String s="121";
+		int k=Integer.parseInt(s);
+		System.out.println(k-9+4*5/6);
+		
+	}
+}
+
